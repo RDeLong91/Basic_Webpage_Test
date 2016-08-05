fbb9b6dc6110e42faa2f82b5df91451ffe38eed5
RDeLong91 <rdelong91@gmail.com>
2016-08-05 01:46:22 -0500

    More tweaks to UI as well as About page
    commit

---
+++ about.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width" , initial-scale="1.0">
+    <title>Web Pages by Ron</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-social.css" rel="stylesheet">
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <link href="css/docs.css" rel="stylesheet">
+    <link href="css/sticky-footer-navbar.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+            Web Pages by Ron
+          </a>
+
+            <div class="navbar-header navbar-right">
+                <ul class="nav navbar-nav">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container-fluid"> <!-- Main content panels -->
+        <div class="row">
+            <div class="col-xs-6 col-md-4">
+                <a href="#" class="thumbnail">
+                    <img src="images/my_self.jpg" alt="images/my_self.jpg">
+                </a>
+            </div>
+            <div class="col-xs-6 col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <h1>Hello!</h1></br>
+                        My name is Ron and I am website developer with an Associate’s degree in Computer Engineering Technology and continuing my
+                        education in computer science in pursuit of my Bachelor's degree.</br></br>
+                        I've been interested in computer science since my freshman year of high school where I took a Visual Basic programming
+                        class. This was my first experience with use cases and from there I became fascinated with the concept of programming. As
+                        I grew older I found a passion for website design because I truly enjoy the puzzle like nature of building a website UI.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
