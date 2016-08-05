6bb35a20699d04276b14ac95c55348b167b053a3
RDeLong91 <rdelong91@gmail.com>
2016-08-05 11:52:31 -0500

    tweaks for responsive design

--- about.php
+++ about.php
@@ -14,44 +14,54 @@
 </head>
 
 <body>
-
     <nav class="navbar navbar-default">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-            Web Pages by Ron
-          </a>
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><img alt="Brand" src="images/brand.png"></a>
+            </div>
 
-            <div class="navbar-header navbar-right">
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li><a href="index.php">Home</a></li>
+                    <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
                     <li><a href="about.php">About</a></li>
                 </ul>
             </div>
+            <!-- /.navbar-collapse -->
         </div>
+        <!-- /.container-fluid -->
     </nav>
 
-    <div class="container-fluid"> <!-- Main content panels -->
+    <div class="container-fluid">
+        <!-- Main content panels -->
         <div class="row">
-            <div class="col-xs-6 col-md-4">
-                <a href="#" class="thumbnail">
-                    <img src="images/my_self.jpg" alt="images/my_self.jpg">
-                </a>
+            <div class="col-xs-6 col-md-5">
+                <img src="images/my_self.jpg" class="img-responsive" alt="Responsive image">
             </div>
-            <div class="col-xs-6 col-md-4">
+            <div class="col-xs-6 col-md-5">
                 <div class="panel panel-default">
                     <div class="panel-body">
                         <h1>Hello!</h1></br>
-                        My name is Ron. I am a website developer based out of Wisconsin with a background in CSS, HTML and Java. I have an associate’s
-                        degree in Computer Engineering Technology and am a continuing student in Computer Science.</br></br>
-                        I’ve been interested in Computer Science since my freshman year of high school where I took some courses in Visual Basic
-                        programming. From these meager beginnings I developed an interest in programmatic things. As my exposure to programming
-                        grew I gained a passion for website design because I enjoy the puzzle like nature of piecing together a user interface.
+                        My name is Ron. I am a website developer based out of Wisconsin with a background in CSS, HTML and Java. I have an associate’s degree in Computer Engineering Technology and am a continuing student in Computer Science.</br>
+                        </br>
+                        I’ve been interested in Computer Science since my freshman year of high school where I took some courses in Visual Basic programming. From these meager beginnings I developed an interest in programmatic things. As my exposure to programming grew I gained
+                        a passion for website design because I enjoy the puzzle like nature of piecing together a user interface.
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
+    <!-- Required for dropdowns -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
 
 </html>
